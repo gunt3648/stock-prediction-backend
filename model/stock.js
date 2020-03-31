3154113a01806128564102111a80cd425ec122e8
Gunt Chanmas <gunt4826@outlook.com>
2020-03-31 14:05:00 +0700

    Add SMA function

--- model/stock.js
+++ model/stock.js
@@ -7,7 +7,8 @@ const SET50 = ['ADVANC.BK', 'AOT.BK', 'AWC.BK', 'BANPU.BK', 'BBL.BK', 'BDMS.BK',
   'EA.BK', 'EGCO.BK', 'GLOBAL.BK', 'GPSC.BK', 'GULF.BK', 'HMPRO.BK', 'INTUCH.BK', 'IRPC.BK', 'IVL.BK',
   'KBANK.BK', 'KTB.BK', 'KTC.BK', 'LH.BK', 'MINT.BK', 'MTC.BK', 'OSC.BK', 'PTT.BK', 'PTTEP.BK', 'PTTGC.BK',
   'RATCH.BK', 'SAWAD.BK', 'SCB.BK', 'SCC.BK', 'TCAP.BK', 'TISCO.BK', 'TMB.BK', 'TOA.BK', 'TOP.BK',
-  'TRUE.BK', 'TU.BK', 'VGI.BK', 'WHA.BK'];
+  'TRUE.BK', 'TU.BK', 'VGI.BK', 'WHA.BK'
+];
 
 stockModel.getSET50 = _ => SET50;
 
@@ -15,8 +16,31 @@ stockModel.isSET50 = symbol => {
   return SET50.indexOf(symbol) > -1;
 };
 
-stockModel.getData = async symbol => {
-  return yahoo.history(symbol, { range: '10y' });
+stockModel.getData = async (symbol, range = '10y') => {
+  return yahoo.history(symbol, {
+    range: range
+  });
+};
+
+stockModel.computeSMA = (data, window_size = 50) => {
+  return new Promise((resolve, reject) => {
+    let r_avgs = [];
+    for (let i = 0; i <= data.length - window_size; i++) {
+      let curr_avg = 0.00,
+        t = i + window_size;
+
+      for (let k = i; k < t && k <= data.length; k++) {
+        curr_avg += data[k].close / window_size;
+      }
+
+      r_avgs.push({
+        set: data.slice(i, i + window_size),
+        avg: curr_avg
+      });
+      avg_prev = curr_avg;
+    }
+    resolve(r_avgs);
+  });
 };
 
 module.exports = stockModel;
--- route/stock.js
+++ route/stock.js
@@ -16,19 +16,56 @@ router.get('/', (req, res) => {
 
 
 router.post('/data', (req, res) => {
-  const symbol = req.body.symbol.toUpperCase();
-  Promise.resolve().then(_ => {
-    if (stockModel.isSET50(symbol) === false) throw new Error('Symbol not found');
+  const action = {
+    do: async (symbol, range) => {
+      const data = await stockModel.getData(symbol, range);
+      return data;
+    },
+    url: '/stock/data',
+    method: 'post'
+  };
 
-    stockModel.getData(symbol).then(data => {
-      res.status(200).send(data);
-    });
-    log.success('/stock/data', "post");
-  })
-    .catch(err => {
-      res.status(500).send('Invalid request data');
-      log.error('/stock/data', err, "post");
-    });
+  stockTemplate(req, res, action);
 });
+
+router.post('/sma', (req, res) => {
+  const action = {
+    do: async (symbol, range) => {
+      const data = await stockModel.getData(symbol, range);
+      const sma = await stockModel.computeSMA(data.records);
+      return sma;
+    },
+    url: '/stock/sma',
+    method: 'post'
+  };
+
+  stockTemplate(req, res, action);
+});
+
+let stockTemplate = (req, res, action) => {
+  const symbol = req.body.symbol.toUpperCase();
+  const range = (req.body.range) ? req.body.range : '10y';
+  Promise.resolve().then(async _ => {
+      if (stockModel.isSET50(symbol) === false) throw new Error('Symbol not found');
+
+      const data = await action.do(symbol, range);
+
+      res.status(200).send(data);
+      log.success(action.url, action.method);
+    })
+    .catch(err => {
+      const errMsg = {
+        msg: 'Invalid input',
+        url: action.url,
+        method: action.method
+      };
+      sendError(res, err, errMsg);
+    });
+};
+
+let sendError = (res, err, errMsg) => {
+  res.status(500).send(errMsg.msg);
+  log.error(errMsg.url, err, errMsg.method);
+};
 
 module.exports = router;
